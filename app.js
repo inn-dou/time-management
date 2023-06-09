c0a9195658253b37a7352f400dfa34f1f941328a
inn-dou <hayato182010519@gmail.com>
2023-06-09 17:15:42 +0900

    republish

--- app.js
+++ app.js
@@ -20,32 +20,7 @@ const connection = mysql.createConnection({
   });
 
   
-//connection test
-  function handleDisconnect() {
-      console.log('INFO.CONNECTION_DB: ');
-      
-      //connection取得
-      connection.connect(function(err) {
-          if (err) {
-              console.log('ERROR.CONNECTION_DB: ', err);
-              setTimeout(handleDisconnect, 1000);
-          }
-      });
-      
-      //error('PROTOCOL_CONNECTION_LOST')時に再接続
-      connection.on('error', function(err) {
-          console.log('ERROR.DB: ', err);
-          if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-              console.log('ERROR.CONNECTION_LOST: ', err);
-              handleDisconnect();
-          } else {
-              throw err;
-          }
-      });
-  }
-  
-  handleDisconnect();
-  //connection test
+
 
 
 app.use(express.static('public'));
